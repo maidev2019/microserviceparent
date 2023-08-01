77149007951da4d5b743ed8e65863cab568098d2
maidev2019 <githubmail2019@gmx.de>
2023-08-01 23:01:21 +0200

    optimzed

--- iban-service/src/main/java/com/maidev/ibanservice/controller/IBANController.java
+++ iban-service/src/main/java/com/maidev/ibanservice/controller/IBANController.java
@@ -1,20 +1,22 @@
 package com.maidev.ibanservice.controller;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.maidev.ibanservice.dto.ErrorResponse;
+import com.maidev.ibanservice.dto.IbanValidationResponse;
+import com.maidev.ibanservice.exception.InvalidCountryException;
 import com.maidev.ibanservice.model.Iban;
 import com.maidev.ibanservice.service.IBANService;
 
 import lombok.AllArgsConstructor;
 
-
-@AllArgsConstructor
 @RestController
+@AllArgsConstructor
 public class IBANController {
     @Autowired
     private final IBANService ibanService;
@@ -23,9 +25,7 @@ public class IBANController {
     public ResponseEntity<Object> generateIBAN(@PathVariable("country") String country) {
         Iban iban = ibanService.generateIBAN(country);
         if (iban == null) {
-            JSONObject errorObject = new JSONObject();
-            errorObject.put("error", "Invalid country");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorObject.toString());
+            return ResponseEntity.badRequest().body(new ErrorResponse("Invalid country"));
         }
 
         JSONObject response = new JSONObject();
@@ -35,16 +35,14 @@ public class IBANController {
     }
 
     @GetMapping("/iban/validate/{ibannumber}")
-    public ResponseEntity<String> validateIBAN(@PathVariable("ibannumber") String ibannumber) {
-        
-        if (ibannumber == null || ibannumber.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid IBAN");
-        }
+    public ResponseEntity<IbanValidationResponse> validateIBAN(@PathVariable("ibannumber") String ibannumber) {
+        boolean isValid = ibanService.validateIBAN(ibannumber);
+        return ResponseEntity.ok(new IbanValidationResponse(isValid));
+    }
 
-        if (!ibanService.validateIBAN(ibannumber)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid IBAN");
-        }
-
-        return ResponseEntity.ok("Valid IBAN");
+    // Exception handler for InvalidCountryException
+    @ExceptionHandler(InvalidCountryException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCountryException(InvalidCountryException ex) {
+        return ResponseEntity.badRequest().body(new ErrorResponse("Invalid country"));
     }
 }
